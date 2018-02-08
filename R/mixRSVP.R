8a2707c1bf50a1c7359b16c576242ffaf89e676c
Alex O. Holcombe <aoholcombe@gmail.com>
2018-02-08 17:43:35 +1100

    working

---
+++ R/mixRSVP.R
@@ -0,0 +1,13 @@
+#' mixRSVP: A package for mixture modeling temporal errors in RSVP data
+#'
+#' The mixRSVP package provides
+#' 
+#' 
+#' @section Understanding the mixture model:
+#' A mixture of the guessing distribution and a Gaussian are fit, see
+#' \href{https://alexholcombe.wordpress.com/2015/04/05/reporting-items-from-a-stream-and-mixture-modeling-to-reveal-buffering-and-a-bottleneck/}{Alex's blogpost} 
+#' To understand the relationship of latency to the exact timing of the cued letter, see
+#' \href{https://github.com/alexholcombe/MixtureModelRSVP/blob/master/mixtureModeling/docs/understandingLatency.png}{understandingLatency}
+#' @docType package
+#' @name mixRSVP
+NULL

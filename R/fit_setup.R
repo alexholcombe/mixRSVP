34a36573cfd224c574d352f109d1eda820167435
Alex O. Holcombe <aoholcombe@gmail.com>
2019-04-19 15:31:37 -0400

    don't know what the changes are

--- R/fit_setup.R
+++ R/fit_setup.R
@@ -27,7 +27,9 @@ parametersGuess<- function( lowerBounds, upperBounds ) {
 #' @export
 parameterBounds<- function() {
   muBound <- 4   #will only consider -4 to +4 for mu
-  sigmaBound <- 4 #will only consider 0 to 4 for sigma
+  sigmaBound <- 3 #will only consider 0 to 3 for sigma, otherwise both I (backwards ltrs P2E2)
+                  #and Charlie get Ss with sigmas of 4 that are very bad fits, too hard to distinguish apparently
+                  #from guessing
 
   # If fit tries negative sigma values
   # Useful number for when limits can't be exactly zero but can be anything larger
